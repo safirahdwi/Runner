738fe167767114f43ebdefb771f857e5fdf7ad98
safirahdwi <safirah988@gmail.com>
2018-12-27 21:46:04 +0700

    asyap

---
+++ Runner/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+    public float speed;
+    public GameObject effect;
+    
+    public GameObject explosionSound;
+
+	void Update () 
+	{
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) {
+			Instantiate(explosionSound, transform.position, Quaternion.identity);
+			Instantiate(effect, transform.position, Quaternion.identity);
+			// player nyawa berkurang
+            other.GetComponent<Player>().health--;
+            other.GetComponent<Player>().camAnim.SetTrigger("shake");
+            Destroy(gameObject);
+        }   
+    }
+}
